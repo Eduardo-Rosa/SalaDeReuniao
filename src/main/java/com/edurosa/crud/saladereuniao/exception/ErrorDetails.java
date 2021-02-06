61c386301b61459b62f280b3404ae5e5d91c79eb
Eduardo-Rosa <edu.betim@aol.com>
2021-02-06 10:55:11 -0300

    atualizando backend

---
+++ src/main/java/com/edurosa/crud/saladereuniao/exception/ErrorDetails.java
@@ -0,0 +1,38 @@
+package com.edurosa.crud.saladereuniao.exception;
+
+import java.util.Date;
+
+public class ErrorDetails {
+
+	private Date timestamp;
+	private String message;
+	private String details;
+	
+	
+	public ErrorDetails(Date timestamp, String message, String details) {
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public String getDetails() {
+		return details;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/edurosa/crud/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.edurosa.crud.saladereuniao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edurosa.crud.saladereuniao.model.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+	
+	
+}
